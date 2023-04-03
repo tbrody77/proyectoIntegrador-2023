0acfb7fd5534669e112710f9b8b009bc572c6ed7
tbrody77 <tbrody@udesa.edu.ar>
2023-04-03 10:27:21 -0300

    database

---
+++ db/zapatillas.js
@@ -0,0 +1,58 @@
+const datos = { 
+    lista: [
+    {
+        id:1,
+        nombre: "Air Force 1",
+        marca: "Nike"
+    },
+    {
+        id: 2,
+        nombre: "Forum Bad Bunny",
+        marca: "Adidas"
+    },
+    {
+        id: 3,
+        nombre: "Air Jordan 1 Mocha",
+        marca: Nike,
+    },
+    {
+        id: 4,
+        nombre: "220",
+        marca: "New Balance"
+
+    },
+    {
+        id: 5,
+        nombre: "Dunk Low Panda",
+        marca: "Nike"
+    },
+    {
+        id: 6,
+        nombre: "Yeezy 350 Desert Sage",
+        marca: "Adidas"
+    },
+    {
+        id:7,
+        Nombre: "All Star",
+        marca:"Converse"
+    },
+
+    {
+        id: 8,
+        nombre: "Air Max 97" ,
+        marca: "Nike",
+    },
+    {
+        id:9,
+        nombre:"Yeezy Slides",
+        marca:"Adidas"
+    },
+    {
+        id: 10,
+        nombre: "Old Skool",
+        marca: "Vans"
+    }
+
+]
+}
+module.exports = datos
