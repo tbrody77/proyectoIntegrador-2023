d3db2c3854d0e957fc02e74b7adf0accf2efd3e6
tbrody77 <tbrody@udesa.edu.ar>
2023-04-03 11:58:45 -0300

    data actualizada

---
+++ db/index.js
@@ -0,0 +1,70 @@
+const data = { 
+    productos: [
+    {
+        nombreProducto: "Air Force 1",imagen: "/images/airforce1.jpeg" ,
+        descripcion: "Nike",fechaCarga: "03/04/2023",
+        alt:"Zapatilla",comentario: "Muy comodas"
+    },
+    {
+        nombreProducto: "Forum Bad Bunny",descripcion: "Adidas",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Hermosas",imagen:"/images/BBforum.jpeg",
+    },
+    {
+        nombreProducto: "Jordan 1 Mocha",descripcion: "Nike",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Tremendas",imagen:"/images/JordanMocha.jpeg",
+        
+    },
+    {
+        nombre: "220",descripcion: "NewBalance",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Combinan muy bien",imagen:"/images/newBalance2002.jpeg",
+    },
+    {   nombre: "Dunk Low Panda",descripcion: "Nike",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Muy quemadas",imagen:"/images/panda.jpeg",
+        },
+    {    nombre: "Yeezy 350 Desert Sage",descripcion: "Adidas",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Son reflectivas",imagen:"/images/JordanMocha.jpeg",
+        
+    },
+    {
+        id:7,
+        Nombre: "All Star",descripcion: "Converse",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Que facheras que son", imagen:"/images/allStar.jpeg",
+        
+    },
+
+    {   nombre: "Air Max 97" , descripcion: "Nike",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Muy turras",imagen:"/images/airmax97.jpeg",
+        
+    },
+    {   nombre:"Yeezy Slides",descripcion: "Adidas",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Es como pisar nubes",imagen:"/images/slides.jpeg",
+        
+    },
+    {    nombre: "Old Skool",descripcion: "Vans",
+        fechaCarga:"03/04/2023",alt: "Zapatilla",
+        comentario: "Tremendas",imagen:"/images/vans.jpeg",
+    }
+],
+comentarios: [
+    {nombreUsuario: "Tom",textoComentario: "Tremendo Producto", imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Martín",textoComentario: "Historicas",imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Martina", textoComentario: "Excelente calidad!",   imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Sebastián",textoComentario: "Horribles", imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Sofía",textoComentario: "Me las compro si o si!", imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Dante",textoComentario: "No me gustan nada jajajaja", imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Candela",textoComentario: "Las quiero", imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Eitán",textoComentario: "Las necesito", imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Gonzalo",textoComentario: "Combinan con todo", imagenPerfil: "/default-image.png"},
+    {nombreUsuario: "Nico",textoComentario: "Dame mil", imagenPerfil: "/default-image.png"},
+],
+}
+
+module.exports = data
