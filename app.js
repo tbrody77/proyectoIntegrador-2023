a15a10d15069adecbd3f924ad0e31263b33c90b5
Tomas Fontana <tomifontana@hotmail.com>
2023-05-23 14:51:50 -0300

    agregando el modulo de session

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var productRouter = require('./routes/product');
 
 var app = express();
 
+const session = require('express-session');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
